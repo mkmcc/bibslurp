1d3bff91c692cd6f9514cc8d9823c4355af56bd1
Mike McCourt <mkmcc@astro.berkeley.edu>
2013-08-10 17:43:15 -0700

    remove last dependencies on lynx
    rather than using lynx --dump, the code instead downloads the URL and
    uses some regexps to parse out authors, titles, links, publication
    dates, etc...
    
    this makes me a little nervous, and it will break badly if ADS ever
    changes their output format.  but this produces more 'emacs-y' output
    and it doesn't require users to have the lynx browser installed.

--- bibslurp.el
+++ bibslurp.el
@@ -52,30 +52,32 @@
 ;; 1. long-term goal: replace lynx calls with internal emacs functions
 
 ;;; Code:
-
-;;; utility functions
-
-(defun bibslurp/s-right (len s)
-  "Returns up to the LEN last chars of S.
-copied from s.el (https://github.com/magnars/s.el)"
-  (let ((l (length s)))
-    (if (> l len)
-        (substring s (- l len) l)
-      s)))
-
+(require 's)
+(require 'dash)
 
 ;;; start by making a rudimentary web browser
+;; define font-lock faces
+(make-face 'bibslurp-number-face)
+(make-face 'bibslurp-name-face)
+(make-face 'bibslurp-score-face)
+(make-face 'bibslurp-date-face)
+(make-face 'bibslurp-author-face)
+(make-face 'bibslurp-title-face)
 
-(defvar bibslurp-link-regexp "\\[\\([0-9]+\\)\\]"
-  "Regular expression that tells bibslurp what links look like.
-The parenthesized subexpression is the unique
-string (representing a base 10 number) denoting a link")
+(set-face-attribute 'bibslurp-number-face nil
+                    :inherit 'font-lock-string-face)
+(set-face-attribute 'bibslurp-score-face nil
+                    :inherit 'font-lock-comment-face)
+(set-face-attribute 'bibslurp-author-face nil
+                    :inherit 'font-lock-builtin-face)
+(set-face-attribute 'bibslurp-title-face nil
+                    :inherit 'font-lock-string-face)
+(set-face-attribute 'bibslurp-name-face nil
+                    :slant 'italic)
+(set-face-attribute 'bibslurp-date-face nil
+                    :inherit 'font-lock-variable-name-face)
 
-(defvar bibslurp-font-lock-keywords
-  `((,bibslurp-link-regexp . font-lock-keyword-face))
-  "Font lock for `bibslurp-mode'.
-Only numbered links are fontified.")
-
+;; key bindings
 (defvar bibslurp-mode-map
   (let ((map (make-keymap)))
     (suppress-keymap map)
@@ -91,16 +93,14 @@ Only numbered links are fontified.")
     map)
   "Keymap for bibslurp mode.")
 
-(define-derived-mode bibslurp-mode fundamental-mode "Bib Slurp"
+(define-derived-mode bibslurp-mode fundamental-mode "BibSlurp"
   "Major mode for perusing ADS search results and slurping bibtex
 entries to the kill-ring.  This is pretty specific, so you should
 only enter the mode via `bibslurp-query-ads'.  Requires the lynx
 browser to the installed.
 
 \\<bibslurp-mode-map>"
-  (use-local-map bibslurp-mode-map)
-  (set (make-local-variable 'font-lock-defaults)
-       '(bibslurp-font-lock-keywords)))
+  (use-local-map bibslurp-mode-map))
 
 (defun bibslurp/follow-link (number)
   "take a link number and return the corresponding url as a
@@ -110,20 +110,10 @@ buffer doesn't conform to the expected \"lynx --dump\" format."
   (interactive "P")
   (let* ((link-number
           (if (stringp number)
-              number
-            (number-to-string number)))
-         (link-regex
-          (concat "\\s-*"
-                  link-number
-                  "\\." "\\s-*"
-                  "\\(http://.*\\)$")))
-    (save-excursion
-      (if (and (re-search-forward "^References$")
-               (re-search-forward link-regex nil t))
-          (match-string-no-properties 1)))))
+              (string-to-number number)
+            number)))
+    (nth (- link-number 1) bibslurp/link-list)))
 
-
-;;; stuff specific to ads
 
 (defun bibslurp/build-ads-url (search-string)
   "Helper function which turns a search string (e.g. \"^Quataert
@@ -136,6 +126,9 @@ buffer doesn't conform to the expected \"lynx --dump\" format."
             (replace-regexp-in-string " " url-sep search-string)
             url-end)))
 
+(defvar bibslurp/link-list nil
+  "list of abstract URLs for the current search.")
+
 ;;;###autoload
 (defun bibslurp-query-ads (&optional search-string)
   "Interactive function which asks for a search string and sends
@@ -146,14 +139,88 @@ abstract link and hitting enter.  You can exit the mode at any
 time by hitting 'q'."
   (interactive (list (read-string "Search string: ")))
   (let ((search-url (bibslurp/build-ads-url search-string))
-        (buf (get-buffer-create "ADS Search Results")))
+        (buf (get-buffer-create "ADS Search Results"))
+        (inhibit-read-only t)
+        (clean-list))
+    (with-temp-buffer ;(get-buffer-create "ADS Scratch")
+      ;(erase-buffer)
+      (url-insert-file-contents search-url)
+      (setq clean-list (-map 'clean-entry (read-table)))
+      (setq bibslurp/link-list '())
+      (--map (add-to-list 'bibslurp/link-list (car (last it)) t) clean-list))
     (with-current-buffer buf
-      (call-process "lynx" nil t nil "-dump" search-url)
-      (goto-char (point-min))
+      (erase-buffer)
+      (insert "ADS Search Results for \""
+              (propertize search-string 'face 'font-lock-string-face)
+              "\"\n\n\n\n")
+      (save-excursion
+        (insert
+         (mapconcat 'identity
+                    (--map (apply 'print-entry it) clean-list)
+                    "\n\n\n\n")))
       (bibslurp-mode))
     (window-configuration-to-register :bibslurp-window)
     (switch-to-buffer buf)
     (delete-other-windows)))
+
+(defun read-table ()
+  (goto-char (point-min))
+  ;; search results are printed in a <table> element.  annoyingly, one
+  ;; result actually spans *two* adjacent table rows, so we keep a
+  ;; temp variable to store and combine them.
+  (re-search-forward "<table>")
+  (let ((rows '())
+        (temp '()))
+    ;; find the next <tr>...</tr> block
+    (while (re-search-forward "<tr>" nil t)
+      (let ((end (save-excursion
+                   (re-search-forward "</tr>")
+                   (point)))
+            (data '()))
+        ;; populate data with the <td>...</td> entries
+        (while (re-search-forward "<td[^>]*>\\(.*?\\)</td>" end t)
+          (add-to-list 'data (match-string-no-properties 1) t))
+        ;; search results start with a number.  if this is a new
+        ;; search result, store it in the temp variable.  otherwise,
+        ;; if temp is non-nil, this is the continuation of a search
+        ;; result.  append them and add to the rows list.
+        (cond
+         ((and (car data) (s-numeric? (car data)))
+          (setq temp data))
+         (temp
+          (add-to-list 'rows (append temp data) t)
+          (setq temp '())))))
+    rows))
+
+(defun clean-entry (entry)
+  ""
+  (let ((num       (nth 0 entry))
+        (link-data (nth 1 entry))
+        (score     (nth 3 entry))
+        (date      (nth 4 entry))
+        (authors   (nth 7 entry))
+        (title     (nth 9 entry)))
+    (when (string-match "<a href=\"\\([^\"]+?\\)\">\\([^<]+\\)</a>" link-data)
+      (let ((abs-url (match-string-no-properties 1 link-data))
+            (abs-name (match-string-no-properties 2 link-data)))
+        (list num score abs-name date authors title abs-url)))))
+
+(defun print-entry (num score abs-name date authors title abs-url)
+  (let* ((fmt-num (concat
+                   (make-string (- 3 (length num)) ? )
+                   (format "[%s].  %s"
+                           (propertize num 'face 'bibslurp-number-face)
+                           (propertize abs-name 'face 'bibslurp-name-face))))
+         (fmt-score (propertize (format "(%s)" score) 'face 'bibslurp-score-face))
+         (pad (make-string (- 80 (length fmt-num) (length fmt-score)) ? ))
+         (meta (concat fmt-num pad fmt-score)))
+    (concat meta "\n"
+            (s-truncate 80
+                        (concat (make-string 8 ? )
+                                (propertize (s-right 4 date) 'face 'bibslurp-date-face) " "
+                                (propertize authors 'face 'bibslurp-author-face)))
+            "\n\n"
+            (when title (s-word-wrap 80 title)))))
 
 (defun bibslurp-quit ()
   "Close the bibslurp buffer and restore the previous window
@@ -242,17 +309,17 @@ more general."
 (defun bibslurp/suggest-label ()
   "Parse an abstract page and suggest a bibtex label.  Returns an
 empty string if no suggestion is found."
-  (let ((author
-         (save-excursion
-           (goto-char (point-min))
-           (if (re-search-forward "Authors:\\s-*\\[\\sw+\\]\\(\\sw+\\)[,;]" nil t)
-               (match-string-no-properties 1))))
-        (date
-         (save-excursion
-           (goto-char (point-min))
-           (if (re-search-forward "Date:\\s-*\\([0-9/]+\\)" nil t)
-               (match-string-no-properties 1)))))
-    (concat author (bibslurp/s-right 4 date))))
+  (save-excursion
+    (goto-char (point-min))
+    (let ((author-regexp
+           "<meta\\s-+name=\"citation_authors\"\\s-+content=\"\\(\\sw+\\)")
+          (date-regexp
+           "<meta\\s-+name=\"citation_date\"\\s-+content=\"\\([0-9/-]+\\)"))
+      (when (re-search-forward author-regexp nil t)
+        (let ((author (match-string-no-properties 1)))
+          (when (re-search-forward date-regexp nil t)
+            (let ((date (match-string-no-properties 1)))
+              (concat author (s-right 4 date)))))))))
 
 (provide 'bibslurp)
 
