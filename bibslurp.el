0bc9f2bdf6101cb248181e81fa3777dff49903e5
Mike McCourt <mkmcc@astro.berkeley.edu>
2015-11-23 10:04:03 -0800

    added a license
    Thanks, Mosè!

--- bibslurp.el
+++ bibslurp.el
@@ -10,6 +10,19 @@
 
 ;; This file is not part of GNU Emacs.
 
+;; bibslurp is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; bibslurp is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with bibslurp.  If not, see http://www.gnu.org/licenses.
+
 ;;; Commentary:
 
 ;; Provides a function `bibslurp-query-ads', which reads a search
